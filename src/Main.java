4a376ae06a811cc65dfa397598f2b2c32d0faca9
“Roshni7605” <roshnimali847@gmail.com>
2024-01-02 17:18:24 +0530

    Added: Check Employee is Present or Absent

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee\n" +
-                "Wage Computation\n" +
-                "Program");
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        /**
+         * UC-1
+         * Check Employee is Present or Absent
+         */
+
+        // Constants
+        int IS_FULL_TIME = 1;
+
+        // Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println(empCheck);
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println(empCheck);
+            System.out.println("Employee is Absent");}
     }
 }
